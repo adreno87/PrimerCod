8968900a91f9ff83e50ede969bb47240501857c1
Alucard <adreno87xd@gmail.com>
2021-06-12 11:23:34 -0500

    Definitivo

--- Division/Program.cs
+++ Division/Program.cs
@@ -10,8 +10,19 @@ namespace Division
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("DIVISION")
-            int numero1, numero2;
+            Console.WriteLine("DIVISION");
+
+            int numero1, numero2, Resultado ;
+            Console.WriteLine("DIVISOR");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("DIVIDENDO");
+            numero2 = int.Parse(Console.ReadLine());
+            Resultado = numero1 / numero2;
+            Console.WriteLine("Resultado es " + Resultado);
+            Console.ReadKey();
+
+
+
         }
     }
 }
